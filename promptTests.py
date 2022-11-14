94a73732b597eb552c3a5afe4002099ae110f511
subha-v <subhavee2@gmail.com>
2022-11-13 18:05:18 -0800

    adding test 2a and refactoring some names

--- promptTests.py
+++ promptTests.py
@@ -1,16 +1,31 @@
 import cohere
 import os
+import re
+from numpy import loadtxt
 
+# Reading the txt files from datasets into python arrays
+my_file = open('./new_datasets/test_1/TURK_Original.txt', 'r')
+data = my_file.read()
+splitting = data.split("\n")
+
+# Calling the prompt iteratively on the dataset
 co = cohere.Client('PsMBI2NlLAh8ZONu3j2x8UNZgEsj9lxZHgojVnn2')
-response = co.generate(
-  model='large',
-  prompt='Simplify this passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn’t the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to “the dusty section of the dictionary” --',
-  max_tokens=100,
-  temperature=0.4,
-  k=0,
-  p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop_sequences=["--"],
-  return_likelihoods='NONE')
-print('Prediction: {}'.format(response.generations[0].text))
+simplified_array = []
+#f = open('TURK_XLarge.txt', 'w')
+
+for i in range (0, len(splitting)-1):
+  response = co.generate(preset="simplifying-sentences-1-bl6ktw", prompt_vars={"sentence": f"{splitting[i]}"})
+  simplified_text = response.generations[0].text
+  with open('TURK_XLarge.txt', 'a') as f:
+    f.write(simplified_text)
+    #f.write('\n')
+  
+  f.close()
+  
+  print(simplified_text)
+  simplified_array.append(simplified_text)
+
+for i in range(0, len(simplified_array)):
+  simplified_array[i] = simplified_array[i].replace('\n', '')
+
+print(simplified_array)
